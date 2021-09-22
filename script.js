160c95419854727378030e87bf5a0326e342fefb
jessicamcg <jessicamcguico@gmail.com>
2021-09-22 12:27:38 -0500

    added score alert when user is done playing

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 
 var choices = ['rock', 'paper','scissors'];
-
 var computer = choices[Math.floor(Math.random()*choices.length)];
-
 var player;
-
 var newGame;
-
-var score;
+var wins = 0;
+var losses = 0;
+var ties = 0;
 
 // game prompts
 var play = window.confirm('would you like to play rock, paper, scissors?');
@@ -20,48 +18,58 @@ while (play) {
         break;
     }
 } 
-window.alert('ok bye!')
+window.alert('ok bye! \r\n' + 
+    wins + ' wins \r\n' +
+    losses + ' losses \r\n' +
+    ties + ' ties');
 
 
 
 //win conditions
 function game(player,computer){
 if (player == computer){
-    window.alert('computer: '+ computer);
-    window.alert('player: '+ player);
-    window.alert('tie');
+    window.alert('computer: '+ computer
+    + '\r\n player: '+ player +
+    '\r\n tie');
+    ties ++;
 } else if (player == 'rock') {
     if (computer == 'paper') {
-        window.alert('computer: '+ computer);
-        window.alert('player: '+ player);
-        window.alert('you lose');
+        window.alert('computer: '+ computer
+        +'\r\n player: '+ player+
+        '\r\n you lose');
+        losses ++;
     }
     if (computer == 'scissors') {
-        window.alert('computer: '+ computer);
-        window.alert('player: '+ player);
-        window.alert('you win');
+        window.alert('computer: '+ computer
+        +'\r\n player: '+ player+
+        '\r\n you win');
+        wins ++;
     }
 } else if (player == 'paper') {   
     if (computer == 'scissors') {
-        window.alert('computer: '+ computer);
-        window.alert('player: '+ player);
-        window.alert('you lose');
+        window.alert('computer: '+ computer
+        +'\r\n player: '+ player+
+        '\r\n you lose');
+        losses ++;
     }
     if (computer == 'rock') {
-        window.alert('computer: '+ computer);
-        window.alert('player: '+ player);
-        window.alert('you win');
+        window.alert('computer: '+ computer
+        +'\r\n player: '+ player+
+        '\r\n you win');
+        wins ++;
     }
 } else if (player == 'scissors') {
     if (computer == 'paper') {
-        window.alert('computer: '+ computer);
-        window.alert('player: '+ player);
-        window.alert('you win');
+        window.alert('computer: '+ computer
+        +'\r\n player: '+ player+
+        '\r\n you win');
+        wins ++;
     }
     if (computer == 'rock') {
-        window.alert('computer: '+ computer);
-        window.alert('player: '+ player);
-        window.alert('you lose');
+        window.alert('computer: '+ computer
+        +'\r\n player: '+ player+
+        '\r\n you lose');
+        losses ++;
     }
 }
 }
